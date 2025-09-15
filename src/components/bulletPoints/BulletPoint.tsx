7f31a08c955254757f3014b455c278b87847bd1a
Michael <michaelseedev@gmail.com>
2025-09-14 20:44:37 -0800

    testing bullet filtering

--- src/components/bulletPoints/BulletPoint.tsx
+++ src/components/bulletPoints/BulletPoint.tsx
@@ -12,8 +12,14 @@ function BulletPoint({
 }: BulletPointProps & { renderIndex: number }) {
   const dispatch = useDispatch();
 
+  //todo 9/14/2025: switch the text check to a value grabbed from a placeholder variable instead of hardcoded.
+  //also, add filtering options in BuilderColumn
   const options = Object.values(
     useSelector((state: RootState) => state.resume.data.bulletPoints)
+  ).filter(
+    (e) =>
+      e.text.length > 0 &&
+      e.text !== "Enter bullet point or choose from dropdown..."
   );
 
   function editData(id: string, text: string) {
