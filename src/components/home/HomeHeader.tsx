80c3ec4f63a54069553363171e34b9920ef02ad9
Michael <michaelseedev@gmail.com>
2025-09-18 01:56:40 -0800

    homepage tweak before break

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function HomeHeader({ title }: Props) {
   return (
-    <div className="flex justify-start items-center bg-blue-500 border-b">
+    <div className="flex justify-start items-center bg-blue-50 border-b">
       <Logo />
       <h1 className="text-2xl tracking-wide font-semibold text-slate-800">
         {title}
