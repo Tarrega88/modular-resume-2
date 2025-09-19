5a8fc3840f68a9bdb52e77c23ec71e2f85563dc4
Michael <michaelseedev@gmail.com>
2025-09-18 18:17:23 -0800

    prepping modal about me window

---
+++ src/components/home/AboutMeWindow.tsx
@@ -0,0 +1,22 @@
+function AboutMeWindow({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen(): void;
+}) {
+  const position = isOpen
+    ? "left-1/2 -translate-x-1/2 outline-2"
+    : "-translate-x-full left-0";
+
+  return (
+    <div
+      className={`absolute bg-slate-600 outline-slate-300 size-3/5 ${position} transition-all duration-200 top-1/2 -translate-y-1/2`}
+    >
+      <div></div>
+      <button onClick={setIsOpen}>Close</button>
+    </div>
+  );
+}
+
+export default AboutMeWindow;
--- src/components/home/HomeSelect.tsx
+++ src/components/home/HomeSelect.tsx
@@ -2,19 +2,37 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/state/store";
 import GenerateResumeButton from "../GenerateResumeButton";
 import PreviousResumeList from "./PreviousResumeList";
+import AboutMeWindow from "./AboutMeWindow";
+import { useState } from "react";
 
 function HomeSelect() {
   const resumeData = useSelector((state: RootState) => state.resume);
 
   const values = Object.values(resumeData.resumeMetaData).reverse();
 
+  const [aboutMeIsOpen, setAboutMeIsOpen] = useState(false);
+
   return (
-    <div className="bg-slate-800 h-full px-10 pt-2 overflow-auto pb-24">
+    <div className="bg-slate-800 h-full px-10 pt-2 overflow-auto flex flex-col">
       <div className="pt-8 pb-8">
         <GenerateResumeButton />
       </div>
 
       <PreviousResumeList values={values} />
+      <div className="h-full flex justify-end items-end">
+        <div className="pb-8">
+          <button
+            className="text-slate-200 p-2 bg-slate-500 rounded-sm"
+            onClick={() => setAboutMeIsOpen(!aboutMeIsOpen)}
+          >
+            About Me
+          </button>
+        </div>
+      </div>
+      <AboutMeWindow
+        isOpen={aboutMeIsOpen}
+        setIsOpen={() => setAboutMeIsOpen(false)}
+      />
       {/* {values.length ? <PreviousResumeList values={values} /> : null} */}
     </div>
   );
