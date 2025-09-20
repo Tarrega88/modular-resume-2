2bed1330932039834ae1e62dd678ae5fa2c0fb92
Michael See <michaelseedev@gmail.com>
2025-09-20 14:43:46 -0800

    styling fix on builder column logo text

--- src/components/builderColumn/BuilderColumnTitle.tsx
+++ src/components/builderColumn/BuilderColumnTitle.tsx
@@ -8,7 +8,7 @@ function BuilderColumnTitle({ title }: Props) {
   return (
     <div className="flex justify-center items-center bg-blue-50">
       <Logo />
-      <div className="text-base sm:text-2xl tracking-wide font-semibold text-slate-800">
+      <div className="text-base sm:text-2xl tracking-wide font-semibold text-slate-800 pr-8 text-center">
         {title}
       </div>
     </div>
