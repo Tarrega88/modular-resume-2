091f19eeb0c196df6eaa14b5d078cdbecd292df7
Michael <michaelseedev@gmail.com>
2025-09-25 02:44:21 -0800

    added last saved component to builder column

---
+++ src/components/builderColumn/LastSaved.tsx
@@ -0,0 +1,22 @@
+import { RootState } from "@/state/store";
+import { formatDate } from "@/utils/formatDate";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+function LastSaved() {
+  const { data } = useSelector((s: RootState) => s.resume);
+  const [updated, setUpdated] = useState(formatDate(new Date()));
+
+  useEffect(() => {
+    setUpdated(formatDate(new Date()));
+  }, [data]);
+
+  return (
+    <div className="bg-blue-50 flex flex-col items-center py-1">
+      <div>Last Saved</div>
+      <div>{updated}</div>
+    </div>
+  );
+}
+
+export default LastSaved;
