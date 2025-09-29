9b03fc374796ccf2e9a59260e833ac01b0628f0b
Michael <michaelseedev@gmail.com>
2025-09-28 22:31:48 -0800

    mobile tweak for bottom ribbon

--- src/components/topMenu/RibbonSlider.tsx
+++ src/components/topMenu/RibbonSlider.tsx
@@ -9,7 +9,7 @@ function RibbonSlider({
   textAdd,
 }) {
   return (
-    <div className="flex gap-2 items-center pr-12">
+    <div className="flex gap-2 items-center pr-12 pt-1 sm:pt-0">
       <div>{children}</div>
       <input
         className="accent-neutral-300 w-44"
