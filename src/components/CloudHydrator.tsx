bd9b616aeccaff215bf4847be2fc12347d6841af
Michael <michaelseedev@gmail.com>
2025-10-02 05:32:17 -0800

    testing cloud pdf change

--- src/components/CloudHydrator.tsx
+++ src/components/CloudHydrator.tsx
@@ -6,17 +6,20 @@ export default function CloudHydrator() {
   const dispatch = useDispatch();
   useEffect(() => {
     (async () => {
-      const token = new URLSearchParams(location.search).get("token");
-      if (token) {
-        const res = await fetch(
-          "https://modular-resume-pdf.88michaelsee.workers.dev/state?token=" +
-            token
-        );
-        const { state } = await res.json();
-        dispatch(hydrate(state));
+      try {
+        const token = new URLSearchParams(location.search).get("token");
+        if (token) {
+          const res = await fetch(
+            "https://modular-resume-pdf.88michaelsee.workers.dev/state?token=" +
+              token
+          );
+          const { state } = await res.json();
+          dispatch(hydrate(state));
+        }
+        if ("fonts" in document) await (document as any).fonts.ready;
+      } finally {
+        (window as any).__renderReady = true;
       }
-      if ("fonts" in document) await (document as any).fonts.ready;
-      (window as any).__renderReady = true;
     })();
   }, []);
   return null;
