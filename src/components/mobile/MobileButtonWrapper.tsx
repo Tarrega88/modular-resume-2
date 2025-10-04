73e74e12f7eece7d7a6514eb12b1d286e6ae2f4f
Michael <michaelseedev@gmail.com>
2025-10-04 04:41:41 -0800

    simplifying layout for mobile button wrapper

--- src/components/mobile/MobileButtonWrapper.tsx
+++ src/components/mobile/MobileButtonWrapper.tsx
@@ -41,41 +41,43 @@ function MobileButtonWrapper({
       style={{ height: "150px" }}
     >
       <div className="flex flex-col h-full justify-end border-r w-16">
-        <div className="flex justify-center items-center size-full">
-          <button onClick={handleMoveBack}>
-            <span className="text-3xl">
-              <TbArrowMoveUp />
-            </span>
-          </button>
-        </div>
-        <div className="border-t flex justify-center items-center size-full">
-          <button
-            onClick={() =>
-              setActiveIndex(activeIndex === 0 ? length - 1 : activeIndex - 1)
-            }
-          >
-            <span className="text-3xl">
-              <IoIosArrowBack />
-            </span>
-          </button>
-        </div>
+        <button
+          onClick={handleMoveBack}
+          className="size-full flex justify-center items-center"
+        >
+          <span className="text-3xl">
+            <TbArrowMoveUp />
+          </span>
+        </button>
+        <button
+          className="size-full flex justify-center items-center border-t"
+          onClick={() =>
+            setActiveIndex(activeIndex === 0 ? length - 1 : activeIndex - 1)
+          }
+        >
+          <span className="text-3xl">
+            <IoIosArrowBack />
+          </span>
+        </button>
       </div>
       {children}
       <div className="flex flex-col h-full justify-end border-l w-16">
-        <div className="flex justify-center items-center size-full">
-          <button onClick={handleMoveForward}>
-            <span className="text-3xl">
-              <TbArrowMoveDown />
-            </span>
-          </button>
-        </div>
-        <div className="border-t flex justify-center items-center size-full">
-          <button onClick={() => setActiveIndex((activeIndex + 1) % length)}>
-            <span className="text-3xl">
-              <IoIosArrowForward />
-            </span>
-          </button>
-        </div>
+        <button
+          className="size-full flex justify-center items-center"
+          onClick={handleMoveForward}
+        >
+          <span className="text-3xl">
+            <TbArrowMoveDown />
+          </span>
+        </button>
+        <button
+          className="size-full flex justify-center items-center border-t"
+          onClick={() => setActiveIndex((activeIndex + 1) % length)}
+        >
+          <span className="text-3xl">
+            <IoIosArrowForward />
+          </span>
+        </button>
       </div>
     </div>
   );
